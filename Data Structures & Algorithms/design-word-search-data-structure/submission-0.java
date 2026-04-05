8d3d4756a66e9f9561a05732db4f6d3f2fa60beb
Aaditya Mathur <104914201+adityamathur456@users.noreply.github.com>
2026-04-05 16:58:32 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.java
@@ -0,0 +1,48 @@
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isWordEnd;
+}
+
+class WordDictionary {
+    TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWordEnd = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int j, TrieNode root) {
+        TrieNode node = root;
+        for (int i = j; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == '.') {
+                for (TrieNode child : node.children) {
+                    if (child != null && dfs(word, i + 1, child)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (node.children[c - 'a'] == null) {
+                    return false;
+                }
+                node = node.children[c - 'a'];
+            }
+        }
+        return node.isWordEnd;
+    }
+}
